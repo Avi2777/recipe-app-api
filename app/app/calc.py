e96ab2ad0a22fec6b854093b351a9d0689e645e3
Aviran Benhamo <aviranbenhamo@gmail.com>
2022-08-18 18:36:40 +0200

    implamenting test logic on deployment

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculato function
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x,y):
+    """Subtract x from y and return result."""
+    return y - x
